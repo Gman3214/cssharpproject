1f8c8e728cacbced40d55aef97d743fcbe9afdef
daniellaleiba <daniellajl@gmail.com>
2021-07-15 17:02:28 +0300

    bgbgbgbg

--- WindowsFormsApp1/forms/games.cs
+++ WindowsFormsApp1/forms/games.cs
@@ -13,34 +13,33 @@ namespace exam
     public partial class frm_games : Form
     {
         public List<WordWImage> gamewords = new List<WordWImage>();
-
-
-
-
         User player = new User();
         int points = 0;
+        static Random rnd = new Random();
+        WordWImage[] randomwordsgame = new WordWImage[3];
+        int index = 0;
 
-        public frm_games(List<WordWImage> gamewords,User player)
+        public frm_games(List<WordWImage> gamewords, User player)
         {
             this.player = player;
             this.gamewords = gamewords;
             InitializeComponent();
         }
-        
+
         public int score
         {
             get { return points; }
-            set { points = points+value; }
+            set { points = points + value; }
         }
         private void pix_game1_Click_1(object sender, EventArgs e)
         {
-            gameone game1 = new gameone(this,gamewords);
+            gameone game1 = new gameone(this, gamewords);
             game1.Show();
         }
 
         private void pix_game2_Click(object sender, EventArgs e)
         {
-            gametwo game2 = new gametwo(this);
+            gametwo game2 = new gametwo(this, gamewords, player);
             game2.Show();
         }
 
@@ -55,5 +54,47 @@ namespace exam
             gamefour game4 = new gamefour(this);
             game4.Show();
         }
+
+        private WordWImage randomfromall()
+        {
+            int y = rnd.Next(gamewords.Count);//אחרת, ניקח מילה רנדומלית מתוך אוסף המילים שלנו. 
+            return gamewords[y];
+        }
+        private void pix_back_Click(object sender, EventArgs e)
+        {
+
+        }
+        private WordWImage randomfromworngs()// פונקציה שבוחרת מילה רנדומלית מתוך אוסף המילים לאיות שלנו
+        {
+            int j = 0;
+            if (player.wrongamewords.Count != 0)
+            //אם אוסף המילים שהמשתמש טעה בהן אינו ריe
+            {
+                int r = rnd.Next(0, player.wrongamewords.Count - 1);
+                //משתנה אינדקס רנדומלי מאוסף המילים שהמשתמש טעה בהן בעבר!!!
+                for (int i = 0; i < gamewords.Count; i++)
+                    if (player.wrongamewords[r] == gamewords[i].wordid)//עוברים על אוסף המילים הכללי ומוצאים את האובייקט בעל אותו id
+                        j = i;//מחזירים מילה שקיימת באוסף המילים שהמשתמש טעה כאובייקט
+            }
+            return gamewords[j];
+        }
+        private void buildrandomarr()// בניית מערך של 3 מילים רנדומליות עבור משחק בודד
+		{
+            for (int i = 0; i < 3; i++)
+			{
+                randomwordsgame[i] = randomfromworngs();
+                for (int j = 0; j < i; j++)
+				{
+                    if (randomwordsgame[i].CompareTo(randomwordsgame[j]) == 0)
+					{
+                        randomwordsgame[i] = randomfromworngs();
+                        j = 0;
+                    }
+				}
+
+            }
+
+        }
+	
     }
 }
